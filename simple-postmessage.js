cc1d8055c24fd33d2310c9ddc2b518ff71d0be42
Leonardo Dutra <leodutra.br@gmail.com>
2014-10-06 19:25:48 -0300

    Fixed IE9- version detection
    Wrong optimization broke the test.

--- simple-postmessage.js
+++ simple-postmessage.js
@@ -53,7 +53,7 @@
       // ie 10- version detection. Useful to fix IE9 and IE8 problem when passing objects as message
       //   http://stackoverflow.com/a/15983064/1260526
       ua = navigator.userAgent.toLowerCase(),
-      IE_VERSION = ~ua.indexOf('msie') && ua.split('msie')[1] | 0; // "~"" -> "x !== -1"
+      IE_VERSION = ~ua.indexOf('msie') && parseInt(ua.split('msie')[1], 10); // "~"" -> "x !== -1"
 	
 	  // Method: window.postMessage
 	  // 
